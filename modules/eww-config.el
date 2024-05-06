c9ca7f1ddf0fe52c4621fd26a304e28e42725bbe
Craig Jennings <c@cjennings.net>
2024-05-06 17:35:40 -0500

    add eww-copy-url to eww-config

--- modules/eww-config.el
+++ modules/eww-config.el
@@ -5,6 +5,22 @@
 
 ;;; Code:
 
+;; -------------------------------- Eww Copy Url -------------------------------
+
+(defun cj/eww-copy-url ()
+  "Copies the eww url to clipboard"
+  (interactive)
+  (when (string= major-mode "eww-mode")
+	(let ((current-url (plist-get eww-data :url)))
+	  (when current-url
+		(kill-new current-url)))))
+
+;; (defun cj/eww-copy-url ()
+;;   "Copies the eww url to clipboard"
+;;   (interactive)
+;;   (when (string= major-mode "eww-mode") ; Ensure we're in eww-mode
+;; 	(kill-new eww-current-url))) ; Copy to clipboard
+
 ;; ------------------------------------ EWW ------------------------------------
 
 (use-package eww
@@ -14,7 +30,8 @@
   (:map eww-mode-map
         ("<" . eww-back-url)    ;; in addition to 'l'
         (">" . eww-forward-url) ;; in addition to 'n'
-        ("i" . eww-toggle-images)
+		("i" . eww-toggle-images)
+		("u" . cj/eww-copy-url)
         ("o" . eww-open-in-new-buffer))
   :config
   (setq shr-use-colors nil)                          ;; respect colors in the html
