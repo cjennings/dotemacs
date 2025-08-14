598d69a542b50f92ed6d03b546bf2dde935faffc
Craig Jennings <c@cjennings.net>
2025-08-14 11:28:26 -0500

    feat(latex): Configure AUCTeX with latexmk and Zathura viewer
    Remove legacy commented code, set up tex and auctex-latexmk packages
    with hooks and options, and document keybindings for macros,
    environments, compilation, and PDF viewing.

--- modules/latex-config.el
+++ modules/latex-config.el
@@ -2,52 +2,38 @@
 ;; author Craig Jennings <c@cjennings.net>
 
 ;;; Commentary:
+;; C-c C-m to enter macros
+;; C-c C-e to enter environment
+
+;; C-c C-c to compile a tex document
+;; C-c C-v to view the resulting pdf
 
 ;;; Code:
 
 ;;;; ----------------------------- Auctex ----------------------------
 
-;; (use-package tex
-;;   :ensure auctex
-;;   :hook
-;;   (LaTeX-mode    . (lambda () (TeX-fold-mode 1)))                    ; automatically activate TeX-fold-mode.
-;;   (TeX-mode-hook . (lambda () (setq TeX-command-default "latexmk"))) ; use latexmk by default
-;;   (LaTeX-mode    . flyspell-mode)                               ; turn on flyspell-mode by default
-;;   ;; (LaTeX-mode    . TeX-PDF-mode)
-;;   ;; (LaTeX-mode . (lambda () (push (list 'output-pdf "Zathura") TeX-view-program-selection)))
-;;   :mode
-;;   ("\\.tex\\'" . latex-mode)
-;;   :config
-;;   (setq TeX-auto-save t)       ; auto save style info when saving buffer
-;;   (setq TeX-parse-self t)      ; parse file after loading if it has no style hook
-;;   (setq TeX-save-query nil)    ; don't ask to save files before starting TeX
-;;   (setq TeX-PDF-mode t)        ; compile to PDF mode, rather than DVI
-;;   (setq-default TeX-master t)) ; Assume the file is the master file itself
+(use-package tex
+  :ensure auctex
+  :defer t
+  :hook
+  (TeX-mode-hook . (lambda () (setq TeX-command-default "latexmk"))) ; use latexmk by default
+  (LaTeX-mode    . (lambda () (TeX-fold-mode 1)))                    ; automatically activate TeX-fold-mode.
+  (LaTeX-mode    . flyspell-mode)                               ; turn on flyspell-mode by default
+  (LaTeX-mode    . TeX-PDF-mode)
+  (LaTeX-mode . (lambda () (push (list 'output-pdf "Zathura") TeX-view-program-selection)))
+  :mode
+  ("\\.tex\\'" . latex-mode)
+  :config
+  (setq TeX-auto-save t)       ; auto save style info when saving buffer
+  (setq TeX-parse-self t)      ; parse file after loading if it has no style hook
+  (setq TeX-save-query nil)    ; don't ask to save files before starting TeX
+  (setq TeX-PDF-mode t)        ; compile to PDF mode, rather than DVI
+  (setq-default TeX-master t)) ; Assume the file is the master file itself
 
-;; ;; use pdftools as viewer
-;; ;; https://emacs.stackexchange.com/questions/21755/use-pdfview-as-default-auctex-pdf-viewer#21764
-;; (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-;;       TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
-;;       TeX-source-correlate-start-server t) ;; not sure if last line is neccessary
-;; ;; to have the buffer refresh after compilation,
-;; ;; very important so that PDFView refreshes itself after compilation
-;; (add-hook 'TeX-after-compilation-finished-functions
-;;           #'TeX-revert-document-buffer)
-
-
-;; https://github.com/tom-tan/auctex-latexmk
-;; You should also add the following line to your .latexmkrc file:
-;; # .latexmkrc starts
-;; $pdf_mode = 1;
-;; # .latexmkrc ends
-
-;; AUCTEX-LATEXMK
-;;
-;; (use-package auctex-latexmk
-;;   :config
-;;   (auctex-latexmk-setup)
-;;   (setq auctex-latexmk-inherit-TeX-PDF-mode t))
-
+(use-package auctex-latexmk
+  :config
+  (auctex-latexmk-setup)
+  (setq auctex-latexmk-inherit-TeX-PDF-mode t))
 
 (provide 'latex-config)
 ;;; latex-config.el ends here
