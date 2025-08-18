190274e57043e8e1c02e1d808c14ccf45a7a6eeb
Craig Jennings <c@cjennings.net>
2025-08-18 08:34:48 -0500

    feat(org-export): load and configure export backends
    - Defer ox loading and require all built-in backends via dolist
    - Set org-export-backends list explicitly
    - Change org-export-with-tasks to include "TODO"
    - Add ox-pandoc, ox-gfm and ox-reveal (configured reveal.js CDN)
    - Remove redundant ox-md and ox-beamer use-package blocks

--- modules/org-export-config.el
+++ modules/org-export-config.el
@@ -8,31 +8,70 @@
 ;; backends for exporting from org-mode
 
 (use-package ox
+  :defer .5
   :ensure nil
   :after org
   :config
+  ;; load every built-in backend
+  (dolist (feat '(ox-ascii
+				  ox-beamer
+				  ox-html
+				  ox-icalendar
+				  ox-latex
+				  ox-md
+				  ox-odt
+				  ox-texinfo
+				  ox-man))
+	(require feat))
+
+  ;; now tell Org exactly which backends to use
+  (setq org-export-backends
+		'(ascii
+		  beamer
+		  html
+		  icalendar
+		  latex
+		  md
+		  odt
+		  org
+		  texinfo
+		  man))
+
+  ;; Other Settings
   (setq org-export-coding-system 'utf-8)        ;; force utf-8 in org
   (setq org-export-headline-levels 6)           ;; export headlines 6 levels deep
   (setq org-export-initial-scope 'subtree)      ;; export the current subtree by default
   (setq org-export-with-author nil)             ;; export without author by default
   (setq org-export-with-section-numbers nil)    ;; export without section numbers by default
   (setq org-export-with-tags nil)               ;; export without tags by default
-  (setq org-export-with-tasks nil)              ;; export without tasks by default
+  (setq org-export-with-tasks '("TODO"))        ;; export with tasks by default
   (setq org-export-with-toc nil))               ;; export without table of contents by default
-
-;; markdown
-(use-package ox-md
-  :ensure nil ;; built-in
-  :after ox)
 
 ;; hugo markdown
 (use-package ox-hugo
   :after ox)
 
-;; beamer presentations
-(use-package ox-beamer
-  :ensure nil ;; built-in
+;; export via pandoc
+(use-package ox-pandoc
   :after ox)
+
+;; github flavored markdown
+(use-package ox-gfm
+  :after ox)
+
+;; reveal .js
+(use-package ox-reveal
+  :after ox
+  :config
+  (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
+
+;; JIRA markup
+;; (use-package ox-jira
+;;   :after ox)
+
+;; ;; Confluence Wiki markup
+;; (use-package ox-confluence
+;;   :after ox)
 
 (provide 'org-export-config)
 ;;; org-export-config.el ends here.
