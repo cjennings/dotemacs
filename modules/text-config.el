cb110aa18ff23d48f03dfdc328e502ac96e7c00d
Craig Jennings <c@cjennings.net>
2025-09-07 16:45:13 -0500

    refactor(text): Improve text editing configuration
    - Move global settings outside of mode-specific functions
    - Standardize use-package declarations with consistent defer values
    - Fix visual-fill-column configuration with proper defaults
    - Remove redundant custom-set-variables
    - Fix typo in file ending line
    - Add documentation in Commentary section

--- modules/text-config.el
+++ modules/text-config.el
@@ -2,59 +2,64 @@
 ;; author Craig Jennings <c@cjennings.net>
 
 ;;; Commentary:
+;;
+;; Configuration for text editing features including:
+;; - Basic text mode settings (visual line mode, indentation, spacing)
+;; - Text manipulation (move-text, expand-region, change-inner)
+;; - Selection behavior (delete-selection-mode)
+;; - Editing tools (edit-indirect, olivetti, accent)
+;; - Visual enhancements (prettify-symbols, visual-fill-column)
 
 ;;; Code:
 
 ;; ------------------------------- Text Settings -------------------------------
 
-(defun cj/text-mode-settings ()
-  "Personal settings for \='text-mode\='."
-  (turn-on-visual-line-mode)            ;; wrap text in text modes (additional modes set elsewhere)
-  (setq-default indent-tabs-mode nil)   ;; indentation should not insert tabs
-  (setq sentence-end-double-space nil)) ;; in the 21st century, sentences may end w/ a single space
-(add-hook 'text-mode-hook 'cj/text-mode-settings)
+;; Global text settings
+(setq-default indent-tabs-mode nil)   ;; indentation should not insert tabs
+(setq require-final-newline nil)      ;; don't require newlines at the end of files
+(setq sentence-end-double-space nil)  ;; in the 21st century, sentences may end w/ a single space
 
-;; don't require newlines at EOF
-(setq require-final-newline nil)                        ;; don't require newlines at the end of files
-(custom-set-variables '(require-final-newline nil))     ;; some major modes read newline setting from custom
+(defun cj/text-mode-settings ()
+  "Personal settings for `text-mode'."
+  (turn-on-visual-line-mode))        ;; wrap text in text modes
+(add-hook 'text-mode-hook 'cj/text-mode-settings)
 
 ;; --------------------------------- Move Text ---------------------------------
 ;; move the current line or selected region up or down in the buffer
 
 (use-package move-text
-  :defer .5
+  :defer 0.5
   :bind
-  ("C-<up>" . move-text-up)
-  ("C-<down>" . move-text-down)
-  :config
-  (move-text-default-bindings))
+  (("C-<up>" . move-text-up)
+   ("C-<down>" . move-text-down)))
 
 ;; ------------------------------- Expand Region -------------------------------
 ;; increase the region by semantic units
 
 (use-package expand-region
-  :defer .5
+  :defer 0.5
   :bind
-  ("M-=" . er/expand-region)
-  ("C->" . er/expand-region)
-  ("M--" . er/contract-region)
-  ("C-<" . er/contract-region))
+  (("M-=" . er/expand-region)
+   ("C->" . er/expand-region)
+   ("M--" . er/contract-region)
+   ("C-<" . er/contract-region)))
 
 ;; ---------------------------- Change Inner / Outer ---------------------------
 ;; change inner and outer, just like in vim.
 
 (use-package change-inner
-  :defer .5
+  :defer 0.5
   :bind (("C-c i" . change-inner)
-		 ("C-c o" . change-outer)))
+         ("C-c o" . change-outer)))
 
 ;; ------------------------------ Delete Selection -----------------------------
 ;; delete the region on character insertion
 
 (use-package delsel
   :ensure nil ;; built-in
-  :defer .5
-  :custom (delete-selection-mode t))
+  :defer 0.5
+  :config
+  (delete-selection-mode t))
 
 ;; ------------------------------- Edit Indirect -------------------------------
 ;; edit selection in new buffer, C-c to finish; replaces with modifications
@@ -78,7 +83,7 @@
 								 ("#+begin_src" . "λ")
 								 ("#+begin_src" . "λ")
 								 ("#+end_src" . "λ")
-								 ("#+begin_quote" . " ")
+                                 ("#+begin_quote" . "")
 								 ("#+end_quote" . "")
 								 ("lambda" . "λ"))))))
 
@@ -105,8 +110,12 @@
 ;; text wrapping
 
 (use-package visual-fill-column
-  :defer .5
-  :demand t)
+  :defer 0.5
+  :config
+  (setq-default visual-fill-column-center-text t)
+  (setq-default visual-fill-column-width 100)
+  :hook
+  (visual-line-mode . visual-fill-column-mode))
 
 (provide 'text-config)
-;;; text-config.el ends heref
+;;; text-config.el ends here
