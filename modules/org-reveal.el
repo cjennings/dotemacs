9da3f4166921bfaf5af53413d6f9fdde8a98edd0
Craig Jennings <c@cjennings.net>
2025-09-15 22:18:16 -0500

    adding chrono-tools and org-reveal

---
+++ modules/org-reveal.el
@@ -0,0 +1,17 @@
+;;; org-reveal.el --- org presentations with reveal.js -*- coding: utf-8; lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; https://github.com/emacsmirror/org-re-reveal
+;;
+;;; Code:
+
+(use-package org-re-reveal
+  :defer 1)
+
+
+(use-package ox-reveal
+  :defer 1)
+
+(provide 'org-reveal)
+;;; org-reveal.el ends here.
