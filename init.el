c77d6bff0e887f22932077f97c2be594830b32b2
Craig Jennings <c@cjennings.net>
2025-10-15 13:30:25 -0500

    add quick video capture to init

--- init.el
+++ init.el
@@ -76,6 +76,7 @@
 (require 'mail-config)           ;; email using mu4e and org-msg
 (require 'markdown-config)
 (require 'pdf-config)            ;; pdf display settings
+(require 'quick-video-capture)   ;; desktop and/or audio recording via ffmpeg
 (require 'show-kill-ring)        ;; displays and facilitates pasting from history
 (require 'video-audio-recording) ;; desktop and/or audio recording via ffmpeg
 (require 'weather-config)        ;; utility to display the weather
