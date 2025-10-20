f3fd8886f921e3dd4560df0095828bea5b4846f8
Craig Jennings <c@cjennings.net>
2025-10-19 23:31:30 -0500

    removed chess from games-config

--- modules/games-config.el
+++ modules/games-config.el
@@ -1,12 +1,19 @@
 ;;; games-config.el --- emacs games -*- lexical-binding: t; coding: utf-8; -*-
 ;; author Craig Jennings <c@cjennings.net>
-
+;;
 ;;; Commentary:
+;;
+;; Configuration for games playable within Emacs. Available games include:
+;;
+;; - Malyon for playing interactive fiction and text adventures in Z-machine format
+;;  (stories directory: ~/sync/org/text.games/)
+;; - 2048 number-tile puzzle game
+;;
+;; Access games by using use the games menu with Shift-Alt-G or use via ~M-x <game-name>~
+;;
 ;; The games menu is the easiest entry. "Shift-Alt-G" will get you there. Enjoy!
 ;;
-
 ;;; Code:
-
 
 ;; ----------------------------------- Malyon ----------------------------------
 ;; text based adventure player
@@ -20,41 +27,6 @@
 ;; combine numbered tiles to create the elusive number 2048.
 (use-package 2048-game
   :defer 1)
-
-;; ----------------------------------- Chess -----------------------------------
-;; play the 64 squares and checkmate the opponent's king
-;; (use-package chess
-;;   :defer 1
-;;   :config
-;;   (setq chess-default-display 'chess-images)
-;;   (setq chess-images-directory
-;; 		(concat user-emacs-directory "assets/chess/pieces/xboard/"))
-;;   (setq chess-images-dark-color "#779556")
-;;   (setq chess-images-light-color "#EBECD0")
-;;   (setq chess-images-default-size 100)
-;;   (setq chess-full-name user-whole-name)
-;;   (setq chess-default-engine 'chess-fruit))
-
-
-;; Notes from source code
-;; If you'd like to view or edit Portable Game Notation (PGN) files,
-;; `chess-pgn-mode' provides a text-mode derived mode which can display the
-;; chess position at point.
-
-;; To improve your chess ability, `M-x chess-tutorial' provides a simple knight
-;; movement exercise to get you started, and `M-x chess-puzzle' can be used
-;; to solve puzzle collections in EPD or PGN format.
-;; The variable `chess-default-display' controls which display modules
-;; are tried when a chessboard should be displayed.  By default, chess-images
-;; is tried first.  If Emacs is not running in a graphical environment,
-;; chess-ics1 is used instead.  To enable the chess-plain display module,
-;; customize `chess-default-display' accordingly.
-
-;; Once this is working, the next thing to do is to customize
-;; `chess-default-modules'.  This is a list of functionality modules used
-;; by chess.el to provide additional functionality.  You can enable or
-;; disable modules so that Emacs Chess better suites your tastes.
-;; Those modules in turn often have configuration variables, and
 
 (provide 'games-config)
 ;;; games-config.el ends here.
