31614cd9e39ab246b72a4fca6533123c8f6e42f2
Craig Jennings <c@cjennings.net>
2025-10-23 00:03:49 -0500

    docs:games-config: Update commentary on game packages
    Streamline the commentary section by removing redundant descriptions
    and instructions for accessing games. Focus on configuring game
    packages instead of listing individual games.

--- modules/games-config.el
+++ modules/games-config.el
@@ -3,15 +3,11 @@
 ;;
 ;;; Commentary:
 ;;
-;; Configuration for games playable within Emacs. Available games include:
+;; Configuration for game packages.
 ;;
 ;; - Malyon for playing interactive fiction and text adventures in Z-machine format
 ;;  (stories directory: ~/sync/org/text.games/)
 ;; - 2048 number-tile puzzle game
-;;
-;; Access games by using use the games menu with Shift-Alt-G or use via ~M-x <game-name>~
-;;
-;; The games menu is the easiest entry. "Shift-Alt-G" will get you there. Enjoy!
 ;;
 ;;; Code:
 
