43eb08a5b9b4f04ed94dd1684615b7ee6c66cd6c
Craig Jennings <c@cjennings.net>
2025-10-29 08:59:42 -0500

    refactor:script: simplify elisp compiled file deletion script
    Remove user confirmation and file listing prompt. Simplify the
    script by directly deleting .eln and .elc files in the specified
    location and confirming completion.

--- scripts/delete-elisp-compiled-files.sh
+++ scripts/delete-elisp-compiled-files.sh
@@ -2,23 +2,6 @@
 
 location=$HOME/.emacs.d/
 
-echo ""; echo "You are about to delete emacs lisp compiled files (.eln and .elc) recursively from $location";
-
-# Show the files it will delete
-echo "The following files will be deleted:"
-find $location -type f \( -name "*.eln" -o -name "*.elc" \) -print
-
-
-echo ""; echo ""
-read -p "Are you sure you want to continue? (y/n) " -n 1 -r
-echo    # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Deleting files..."
-    find $location -type f \( -name "*.eln" -o -name "*.elc" \) -exec rm -f {} +
-    echo "Files deleted."
-else
-    echo "Operation cancelled."
-fi
-
-echo ""; echo ""
+echo "Deleting emacs lisp compiled files (.eln and .elc) from $location..."
+find $location -type f \( -name "*.eln" -o -name "*.elc" \) -exec rm -f {} +
+echo "Done."
